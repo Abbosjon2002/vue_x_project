ac59ebd5578a3e7e49f546a57118a8c4eeb124b2
Abbosjon <abbosbekanvarov2002@gmail.com>
2023-04-10 06:19:48 +0500

    navigation done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,31 +4,42 @@
       <RouterLink to="/" class="d-flex align-items-center text-dark text-decoration-none">
         <img :src="logo" alt="logo" class="w-25">
       </RouterLink>
+      <template v-if="isLoggedIn">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMfaVtR50PIe3narGDtBb9CHruowCn4RYjztsuoFU&s" alt="acc" class="m-3" style="width: 30px">
+        <RouterLink :to="{name: 'home'}" class="me-3 py-2  text-primary text-uppercase  text-decoration-none" href="#">{{user.username }}</RouterLink>
+      </template>
 
-      <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <RouterLink :to="{name: 'login'}" class="me-3 py-2 text-dark text-decoration-none" href="#">Login</RouterLink>
-        <RouterLink :to="{name: 'register'}" class="me-3 py-2 text-dark text-decoration-none" href="#">Register</RouterLink>
-      </nav>
+      <template v-if="!isLoggedIn">
+        <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+          <RouterLink :to="{name: 'login'}" class="me-3 py-2 text-dark text-decoration-none" href="#">Login</RouterLink>
+          <RouterLink :to="{name: 'register'}" class="me-3 py-2 text-dark text-decoration-none" href="#">Register
+          </RouterLink>
+        </nav>
+      </template>
     </div>
   </header>
 </template>
 
 <script>
 import {logo} from "@/components/constants";
+import {mapState} from "vuex";
 
 export default {
   name: "Navbar",
 
   data() {
-    return{
+    return {
       logo: logo
     }
+  },
+  computed: {
+    ...mapState({
+      user: state => state.auth.user,
+      isLoggedIn: state => state.auth.isLoggedIn
+    })
   }
 }
 </script>
 
 <style scoped>
-.logo{
-  width: 150px;
-}
 </style>
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -4,7 +4,8 @@ import {setItem} from "@/helper/persistanseStorage";
 const state = {
     isLoading: false,
     user: null,
-    error: null
+    error: null,
+    isLoggedIn: null
 }
 
 const mutations = {
@@ -12,27 +13,36 @@ const mutations = {
         state.isLoading = true
         state.user = null
         state.error = null
+        state.isLoggedIn = null
     },
     successRegister(state, payload) {
         state.isLoading = false
         state.user = payload
+        state.isLoggedIn = true
     },
     failureRegister(state, payload) {
         state.isLoading = false
+        state.isLoggedIn = false
         state.error = payload.errors
     },
     startLogin(state){
         state.isLoading = true
         state.user = null
         state.error = null
+        state.isLoggedIn = null
+
     },
     successLogin(state, payload){
         state.isLoading = false
         state.user = payload
+        state.isLoggedIn = true
+
     },
     failureLogin(state, payload){
         state.isLoading = false
         state.error = payload.errors
+        state.isLoggedIn = false
+
     },
 
 }
@@ -60,12 +70,12 @@ const actions = {
             context.commit('startLogin')
             AuthService.login(user)
                 .then(response =>{
-                    context.commit('successRegister', response.data.user)
+                    context.commit('successLogin', response.data.user)
                     setItem('token', response.data.user.token)
                     resolve(response.data.user)
                 })
                 .catch(error => {
-                    context.commit('failureRegister', error.response.data)
+                    context.commit('failureLogin', error.response.data)
                     reject(error.response.data)
                 })
         });
